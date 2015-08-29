32a983383ff85638c83074ffb5c588f329046804
Brian Lu <wahsay@gmail.com>
2015-08-29 00:33:37 -0400

    Use Ruby 1.9 hash syntax

--- blu.rb
+++ blu.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'tilt/haml'
 require 'tilt/sass'
 
-set :haml, :format => :html5
+set :haml, format: :html5
 
 def media_url(uri = nil)
   File.join('//media.blu.pub', url(uri, false))
